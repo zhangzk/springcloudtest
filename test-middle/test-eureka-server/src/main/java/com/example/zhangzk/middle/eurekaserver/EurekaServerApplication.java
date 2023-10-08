540dce141153dd553c3a96eab67baa09f9772de3
zhangzk <zhang_zhaokun@126.com>
2023-10-08 14:20:33 +0800

    eureka server ok

---
+++ test-middle/test-eureka-server/src/main/java/com/example/zhangzk/middle/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.example.zhangzk.middle.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zhangzhaokun
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServerApplication.class, args);
+  }
+
+}
